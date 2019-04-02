c82319329e1aeb2f9b549c3701881fcba4fbe622
Leon Kuhne <lxk1170@rit.edu>
2019-04-01 23:54:43 -0400

    added window moving

--- public/scripts/recipematch.js
+++ public/scripts/recipematch.js
@@ -6,13 +6,21 @@ window.onload = () => {
 
   // autofocus on homepage
   $('.ingredients-page').hover(()=>{
+    $('.ingredients-page').addClass('selected-page')
+    $('.ingredients-page').removeClass('deselected-page')
     $('.ingredients-search').focus()
   }, ()=>{
+    $('.ingredients-page').removeClass('selected-page')
+    $('.ingredients-page').addClass('deselected-page')
     $('.ingredients-search').focusout()
   })
   $('.recipes-page').hover(()=>{
+    $('.recipes-page').addClass('selected-page')
+    $('.recipes-page').removeClass('deselected-page')
     $('.recipe-search').focus()
   }, ()=>{
+    $('.recipes-page').removeClass('selected-page')
+    $('.recipes-page').addClass('deselected-page')
     $('.recipe-search').focusout()
   })
 }
