2967479016aea54ad7bb41c23605c4734473fbee
Leon Kuhne <lxk1170@rit.edu>
2019-04-01 22:36:16 -0400

    added class information for pages

--- routes/search.js
+++ routes/search.js
@@ -2,15 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/recipes', function(req, res, next) {
-  res.render('search', { title: 'Recipes' });
+  res.render('search', { title: 'Recipes', pageStyle: 'recipes-page' });
 });
 
 router.get('/ingredients', function(req, res, next) {
-  res.render('search', { title: 'Ingredients' });
+  res.render('search', { title: 'Ingredients', pageStyle: 'ingredients-page', searchType: 'ingredients_search' });
 });
 
 router.get('/my-recipes', function(req, res, next) {
-  res.render('search', { title: 'My Recipes' });
+  res.render('search', { title: 'My Recipes', pageStyle: 'my-recipes-page' });
 });
 
 module.exports = router;
