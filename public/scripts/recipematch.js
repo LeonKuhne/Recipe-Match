6f9b2053c34a4af152074c0f60538b5a8a84d06a
Leon Kuhne <lxk1170@rit.edu>
2019-04-01 23:38:26 -0400

    added focus on hovering

--- public/scripts/recipematch.js
+++ public/scripts/recipematch.js
@@ -3,4 +3,16 @@ window.onload = () => {
   $('.logo').click(()=>{
     document.location.href="/";
   })
+
+  // autofocus on homepage
+  $('.ingredients-page').hover(()=>{
+    $('.ingredients-search').focus()
+  }, ()=>{
+    $('.ingredients-search').focusout()
+  })
+  $('.recipes-page').hover(()=>{
+    $('.recipe-search').focus()
+  }, ()=>{
+    $('.recipe-search').focusout()
+  })
 }
