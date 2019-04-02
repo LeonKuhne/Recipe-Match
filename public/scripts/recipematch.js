987ae8f8104a9b38d615241183a285896982cb0d
Leon Kuhne <lxk1170@rit.edu>
2019-04-02 00:07:50 -0400

    added more homepage steeze

--- public/scripts/recipematch.js
+++ public/scripts/recipematch.js
@@ -9,18 +9,38 @@ window.onload = () => {
     $('.ingredients-page').addClass('selected-page')
     $('.ingredients-page').removeClass('deselected-page')
     $('.ingredients-search').focus()
+
+    $('.recipes-page').removeClass('deselected-page')
   }, ()=>{
     $('.ingredients-page').removeClass('selected-page')
     $('.ingredients-page').addClass('deselected-page')
     $('.ingredients-search').focusout()
   })
+  
   $('.recipes-page').hover(()=>{
     $('.recipes-page').addClass('selected-page')
     $('.recipes-page').removeClass('deselected-page')
     $('.recipe-search').focus()
+    
+    $('.ingredients-page').removeClass('deselected-page')
   }, ()=>{
     $('.recipes-page').removeClass('selected-page')
     $('.recipes-page').addClass('deselected-page')
     $('.recipe-search').focusout()
+  })
+
+  // links on homepage
+  $('.ingredients-page').click(()=>{
+    $('.ingredients-page').addClass('expand');
+    setTimeout(()=>{
+      document.location.href="/search/ingredients";
+    }, 500)
+  })
+
+  $('.recipes-page').click(()=>{
+    $('.recipes-page').addClass('expand');
+    setTimeout(()=>{
+      document.location.href="/search/recipes";
+    }, 500)
   })
 }
