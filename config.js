0202426aee3c1952b65882650b1338ef27afcbbc
Leon Kuhne <lxk1170@rit.edu>
2019-04-03 18:15:27 -0400

    changed backend to centralize recipes

--- config.js
+++ config.js
@@ -1,7 +1,29 @@
-const config = {
+//  GLOBAL
+var config = {
   server: {
     local: process.env.PORT ? false : true,
     port: process.env.PORT || 5353
+  },
+  results: {
+    recipe: [],
+    ingredients: [{
+      id: 0,
+      picture: 'fresh.png',
+      title: 'Fresh Potato',
+      description: 'Izza big potato with lotz of sauze.',
+      duration: '2 hours',
+      difficulty: 'hard'
+    },
+    {
+      id: 1,
+      picture: 'baked.png',
+      title: 'Baked Potato',
+      description: 'Izza baked potato with lotz of sauze.',
+      duration: '30 min',
+      difficulty: 'easy'
+    }
+    ],
+    myRecipe: []
   }
 }
 
