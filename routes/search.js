318d07d5d46d373f6666b798d049bbeeda13318e
Leon Kuhne <lxk1170@rit.edu>
2019-04-21 22:37:48 -0400

    made searching actually filter

--- routes/search.js
+++ routes/search.js
@@ -2,16 +2,65 @@ var express = require('express');
 var config = require('../config')
 var router = express.Router();
 
+function findRecipe(recipeId) {
+  results = config.results
+  recipes = results.recipes.concat(results.ingredients, results.myRecipe)
+
+  for (let recipe of recipes) {
+    if ('recipe-' + recipe.id === recipeId) {
+      return recipe
+    }
+  }
+
+  console.log('ERROR: found unknown recipe with id: ' + recipeId)
+  return {}
+}
+
+// does list of strings contain query
+function listContains(collection, query) {
+  for(let elem of collection) {
+    if(elem.includes(query)) {
+      return true
+    }
+  }
+  return false
+}
+
+// search filter functionality
+function filterRecipes(collection, query) {
+  let list = []
+  for(let elem of collection) {
+    if((elem.title && elem.title.includes(query)) || (elem.description && elem.description.includes(query)) || listContains(elem.instructions, query)) {
+    	list.push(elem)
+    } else {
+    	for(let ingredient of elem.ingredients) {
+	  if(ingredient.ingredientName && ingredient.ingredientName.includes(query)) {
+	    list.push(elem)
+	  }
+	}
+    }
+  }
+
+  if(list.length == 0)  {
+    return collection
+  }
+
+  return list
+}
+
 router.get('/recipes', function (req, res, next) {
-  res.render('search', { title: 'Recipes', placeholder: 'Search by name...', pageStyle: 'recipes-page', searchType: '', searchResults: config.results.recipes });
+  let results = filterRecipes(config.results.recipes, req.query.query)
+  res.render('search', { title: 'Recipes', placeholder: 'Search by name...', pageStyle: 'recipes-page', searchType: '', searchResults: results });
 });
 
 router.get('/ingredients', function (req, res, next) {
-  res.render('search', { title: 'Ingredients', placeholder: 'Search by ingredients...', pageStyle: 'ingredients-page', searchType: 'ingredients-search', searchResults: config.results.recipes });
+  let results = filterRecipes(config.results.recipes, req.query.query)
+  res.render('search', { title: 'Ingredients', placeholder: 'Search by ingredients...', pageStyle: 'ingredients-page', searchType: 'ingredients-search', searchResults: results });
 });
 
 router.get('/my-recipes', function (req, res, next) {
-  res.render('search', { title: 'My Recipes', placeholder: 'Search my recipes...', pageStyle: 'my-recipes-page', searchType: '', searchResults: config.results.myRecipe });
+  let results = filterRecipes(config.results.myRecipes, req.query.query)
+  res.render('search', { title: 'My Recipes', placeholder: 'Search my recipes...', pageStyle: 'my-recipes-page', searchType: '', searchResults: results });
 });
 
 module.exports = router;
